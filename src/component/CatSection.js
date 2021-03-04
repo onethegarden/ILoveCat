b6235fce3c263c1091a54c61e4c0d180720a150d
onethegarden <onethegarden@gmail.com>
2021-03-04 11:32:15 +0900

    cat section image slide 구현

--- src/component/CatSection.js
+++ src/component/CatSection.js
@@ -6,6 +6,8 @@ export default class CatSection {
     this.$slideSection = document.createElement('div');
     this.$slideSection.className = 'slideSection';
     this.$target.appendChild(this.$slideSection);
+
+    this.$currSlide = 0; //슬라이드 현재 위치
     this.onLoad();
   }
 
@@ -14,23 +16,47 @@ export default class CatSection {
     console.log(this.$data);
     this.render();
   }
-  slide() {}
+
+  slide = (e) => {
+    if (e.target.className !== 'dot') return;
+
+    const wrapper = document.querySelector('.cards-wrapper');
+    const displayArea = wrapper.parentElement.clientWidth;
+
+    if (this.$currSlide === 0 && e.target.id === 'left') return;
+
+    const navi = e.target.id == 'right' ? -1 : +1;
+
+    this.$currSlide = this.$currSlide + navi;
+
+    const pixels = displayArea * this.$currSlide;
+
+    wrapper.style.transform = 'translateX(' + pixels + 'px)';
+  };
 
   render() {
     //this.$randomSection.appendChild();
     const html = `
-    <ul class="sliderContainer">
-        <li class="slideImg">slideImage1</li>
-        <li class="slideImg">slideImage2</li>
-        <li class="slideImg">slideImage3</li>
-    </ul>
-    <div class="navigator">
-      <div class="left"> < </div>
-      <div class="right"> > </div>
+    <div class="display-area">
+      <div class="cards-wrapper">
+        ${
+          this.$data &&
+          this.$data
+            .map((cat) => {
+              return `<div class="card"><img src="${cat.url}" alt="고양이"></div>`;
+            })
+            .join('')
+        }
+      </div> 
+    </div>
+    <div class="dots-wrapper">
+      <button class="dot" id="left"> < </button>
+      <button class="dot" id="right"> > </button>
     </div>
     `;
     this.$slideSection.innerHTML += html;
-    console.log('cat section');
-    this.slide();
+
+    const wrapper = document.querySelector('.slideSection');
+    wrapper.addEventListener('click', this.slide);
   }
 }
